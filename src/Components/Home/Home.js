9043d57b194c92b4674b1d3a410c2d31a91775a2
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 19:11:42 +0600

    user delete method added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,13 +10,36 @@ const Home = () => {
             .then(data => setUsers(data))
     }, [])
 
+    const handleUserDelete = _id => {
+        const proceed = window.confirm('Are you sure you want to delete')
+
+        if (proceed) {
+            console.log('deleting user with id', _id)
+            const url = `http://localhost:5000/user/${_id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = users.filter(user => user._id !== _id)
+                        setUsers(remaining)
+                    }
+                })
+        }
+    }
     return (
         <div>
             <h2>Available Users: {users.length}</h2>
 
             <ul>
                 {
-                    users.map(user => <li>{user.name}:: {user.email}</li>)
+                    users.map(user => <li
+                        key={user._id}>
+                        {user.name}: {user.email}
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+                    </li>)
                 }
             </ul>
         </div>
