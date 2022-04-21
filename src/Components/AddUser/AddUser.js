eceeee0f9190fe78b8c81b844c418a9fad3a03dc
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 17:30:14 +0600

    display user data

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -20,6 +20,8 @@ const AddUser = () => {
             .then(res => res.json())
             .then(data => {
                 console.log('success', data);
+                alert('user added successfully!!!')
+                event.target.reset()
             })
     }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
     return (
         <div>
-            
+            <h2>Available Users: {users.length}</h2>
+
+            <ul>
+                {
+                    users.map(user => <li>{user.name}:: {user.email}</li>)
+                }
+            </ul>
         </div>
     );
 };
