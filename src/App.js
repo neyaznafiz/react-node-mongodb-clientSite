60a9ad348b5f80270d62bed30f09b1d47f97c95b
niiyazobug <niiyaz5555@gmail.com>
2022-04-21 22:36:24 +0600

    update profile method added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddUser from './Components/AddUser/AddUser';
 import Home from './Components/Home/Home';
+import UpdateUser from './Components/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/user/add' element={<AddUser></AddUser>}></Route>
+        <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
 
       </Routes>
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -38,7 +39,11 @@ const Home = () => {
                     users.map(user => <li
                         key={user._id}>
                         {user.name}: {user.email}
+
+                        <Link to={`/update/${user._id}`}> <button>Update</button> </Link>
+
                         <button onClick={() => handleUserDelete(user._id)}>X</button>
+                        
                     </li>)
                 }
             </ul>
